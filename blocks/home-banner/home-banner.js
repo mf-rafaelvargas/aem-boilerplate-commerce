ef4f77fd541dbe76afd1efbb68f98e7b4d0a8d69
Rafael Vargas <rvargas@mcfadyen.com>
2025-04-08 10:30:32 -0300

    Create home banner

---
+++ blocks/home-banner/home-banner.js
@@ -0,0 +1,10 @@
+export default function decorate(block) {
+    block.innerHTML = `
+      <div class="homepage-banner">
+        <a href="/sale">
+          AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        </a>
+      </div>
+    `;
+  }
+  
